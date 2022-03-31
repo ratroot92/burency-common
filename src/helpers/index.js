2def54f11760322b18f154a5513ec10d3bb651a0
malickateeq <malickateeq@gmail.com>
2022-03-31 10:51:53 +0500

    Remove redundant validation files

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,9 @@
 const Response = require("./Response");
-const Validation = require("./Validation");
 const Encryption = require("./Encryption");
 const helpers = require("./helpers");
 
 module.exports = {
     Response,
-    Validation,
     Encryption,
     ...helpers,
 }
