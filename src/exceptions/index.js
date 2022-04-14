78e99939a5a79c16229083196f803637e930ae80
Ahmed Makhlouf <a.makhlouf@vaultspay.ae>
2022-04-14 09:45:31 +0500

    Updates

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,10 +1,6 @@
-const DatabaseConnectionError = require("./DatabaseConnectionError");
-const BurencyError = require("@burency/common/src/exceptions/BurencyError");
 const ApiError = require("./ApiError");
 
 module.exports = 
 {
-	DatabaseConnectionError,
-	BurencyError,
 	ApiError,
 };
