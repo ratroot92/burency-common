cd143b9dd8032620102bd88e3dbe1c31fc688fa1
Ahmed Makhlouf <a.makhlouf@vaultspay.ae>
2022-04-14 15:09:21 +0500

    Updates

--- src/helpers/Encryption.js
+++ src/helpers/Encryption.js
@@ -2,33 +2,33 @@ const bcrypt = require('bcrypt');
 
 class Encryption
 {   
-    /**
+   /**
     * 
     * @returns object
     */
-    static async hash(data = {})
-    {
-        const salt = await bcrypt.genSalt(10);
-        return await bcrypt.hash(data, salt);
-    }
+   static async hash(data = {})
+   {
+      const salt = await bcrypt.genSalt(10);
+      return await bcrypt.hash(data, salt);
+   }
 
    /**
     * 
     * @returns object
     */
-    static async hashCompare(data = {}, hashedData)
-    {
-       return await bcrypt.compare(data, hashedData);
-    }
+   static async hashCompare(data = {}, hashedData)
+   {
+      return await bcrypt.compare(data, hashedData);
+   }
 
-    /**
-     * 
-     * @returns object
-     */
-     static async checkHash(data = {}, hashedData)
-     {
-        return await bcrypt.compare(data, hashedData);
-     }
+   /**
+    * 
+    * @returns object
+    */
+   static async checkHash(data = {}, hashedData)
+   {
+      return await bcrypt.compare(data, hashedData);
+   }
 }
 
 module.exports = Encryption;
