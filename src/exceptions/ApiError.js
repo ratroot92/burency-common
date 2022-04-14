1ed0b04128b8536d331ce6876d7396063547bd41
Ahmed Makhlouf <a.makhlouf@vaultspay.ae>
2022-04-14 20:06:24 +0500

    Updates

--- src/exceptions/ApiError.js
+++ src/exceptions/ApiError.js
@@ -1,11 +1,13 @@
-class AppError extends Error {
-
-    constructor(data) {  
+class AppError extends Error 
+{
+    constructor(data) 
+	{  
 		super(data.message);
 		this.status = data.status;
+		this.message = data.message ?? {};
 		this.data = data.data ?? {};
 		this.accessToken = data.accessToken ?? null;
 	}
-  }
-  
-  module.exports = AppError;
+}
+
+module.exports = AppError;
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,44 +1,47 @@
 const JWT = require("../utils/jwt");
 const { Response } = require("../helpers");
 
-const authenticated = async (req, res, next) => 
+const auth = function (options = {}) 
 {
-    const verifiedToken = JWT.verifyToken(req.headers.accesstoken);
-
-    // 1. Check if token is valid
-    if(!verifiedToken)
+    return async (req, res, next) => 
     {
-        return res.status(401).json( Response.unauthorize({ message: "Invalid token!"}) );
-    }
-
-    // 2. Check if token is valid for certain route
-    if(verifiedToken.validFor)
-    {
-        const path = req.originalUrl.replace(/^\/|\/$/g, '');
-        if(path !== verifiedToken.validFor)
-            return res.status(401).json( Response.unauthorize({ message: "Token is not valid for this route."}) );
-    }
-
-    // // 3. Check if token is not expired
-    // if( verifiedToken.exp <= Date.now() )
-    //     res.json( Response.unauthorize({ message: "Token has been expired."}) );
-
-    // Attach user with the request
-    req.authUser = verifiedToken.data;
+        const verifiedToken = JWT.verifyToken(req.headers.accesstoken);
     
-    var oldSend = res.send;
-    res.send = function(data)
-    {
-        data = JSON.parse(data);
-        if(data.accessToken === null && req.authUser?.authenticated === true)
+        // 1. Check if token is valid
+        if(!verifiedToken)
         {
-            const accessToken =  JWT.getToken({ ...req.authUser });
-            data.accessToken = accessToken;
+            return res.status(401).json( Response.unauthorize({ message: "Invalid token!"}) );
         }
-        res.send = oldSend // set function back to avoid the 'double-send'
-        return res.send(data) // just call as normal with data
+    
+        // 2. Check if token is valid for certain route
+        if(verifiedToken.validFor || options?.checkValidFor)
+        {
+            const path = req.originalUrl.replace(/^\/|\/$/g, '');
+            if(path !== verifiedToken.validFor)
+                return res.status(401).json( Response.unauthorize({ message: "Token is not valid for this route."}) );
+        }
+    
+        // // 3. Check if token is not expired
+        // if( verifiedToken.exp <= Date.now() )
+        //     res.json( Response.unauthorize({ message: "Token has been expired."}) );
+    
+        // Attach user with the request
+        req.authUser = verifiedToken.data;
+        
+        var oldSend = res.send;
+        res.send = function(data)
+        {
+            data = JSON.parse(data);
+            if(data?.accessToken === null && req.authUser?.authenticated === true)
+            {
+                const accessToken =  JWT.getToken({ ...req.authUser });
+                data.accessToken = accessToken;
+            }
+            res.send = oldSend // set function back to avoid the 'double-send'
+            return res.send(data) // just call as normal with data
+        }
+        next();
     }
-    next();
 }
 
-module.exports = authenticated;
+module.exports = auth;
