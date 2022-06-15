da3757b47fee36b6b8714660badc2303eb3dc88e
adil_hussain21 <adil@vaultspay.ae>
2022-06-15 20:43:54 +0500

    Exception handeling changes

---
+++ src/exceptions/LogException.js
@@ -0,0 +1,32 @@
+const { Stream } = require("../stream");
+const { env } = require("../helpers");
+
+class LogException {
+    static async log_exception(req, error) {
+        const { rawHeaders, method, originalUrl, coRelationId } = req;
+        var ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+        var user = req.authUser?.user.email ? req.authUser?.user.email : req.authUser?.user.phone;
+
+        const logData = {
+            coRelationId, level: "error", log_type:"REST_API", service: env("APP_NAME"), user, ipAddress,
+            ...{ request: { rawHeaders, method, originalUrl, ipAddress } }, processingTime: performance.now() - req?.startTime,
+            ...{
+                error: {
+                    message: error.message || "Something went wrong",
+                    stack_trace: error.stack || error
+                }
+            }
+        }
+        const streamServer = new Stream({
+            clientId: env("KAFKA_CLIENT_ID"),
+            brokers: [env("KAFKA_BROKERS")]
+        });
+        await streamServer.produce(logData,
+            {
+                topic: "LOG_ERROR.REST_API"
+            });
+    }
+
+}
+
+module.exports = LogException;
