adf6da098df8041682cda1b0f455ab38c89a4fa5
adil_hussain21 <adil@vaultspay.ae>
2022-06-30 13:24:52 +0500

    route wrapper in helper

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -42,7 +42,45 @@ const exists = async (value, options) =>
     
 }
 
+const routerWrapper = (router) => 
+{
+    const _route = router.route.bind(router);
+    const methodsToWrap = ['get', 'post', 'patch', 'put', 'delete'];
+    router.route = function (path) {
+        const route = _route(path);
+        for (const method of methodsToWrap) {
+            if (route[method]) {
+
+                route[method] = routeContainer(route[method]);
+            }
+        }
+        return route;
+    };
+}
+
+const routeContainer = (originRouterMethod) => 
+{
+    return function () {
+        const originMiddlewares = [...arguments];
+        const wrappedMiddlewares = originMiddlewares.map(fn => {
+            if (typeof fn !== `function`) {
+                return fn;
+            }
+
+            return async function (req, res, next) {
+                try {
+                    await fn.apply(null, arguments);
+                } catch (err) {
+                    next(err);
+                }
+            };
+        });
+        originRouterMethod.call(this, wrappedMiddlewares);
+    };
+}
+
 module.exports = {
     getPath,
-    env
+    env,
+    routerWrapper
 }
