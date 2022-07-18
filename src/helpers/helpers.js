ace91dbc21c3cf7b2308c53f13d177548b4168fa
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2022-07-18 13:38:15 +0500

    add settings helper

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,86 +1,103 @@
-const getPath = (fullUrl) =>
-{
-    return fullUrl.replace(/^\/|\/$/g, "");
-}
-const env = (key, defaultValue) =>
-{
-    if(!key in process.env)
-    {
-        if(defaultValue) return defaultValue;
-        throw Error(`${key} not found in ENV variables!`);
-    }
+const path = require('path');
 
-    // Access Cache if enabled
+const getPath = (fullUrl) => {
+	return fullUrl.replace(/^\/|\/$/g, '');
+};
+const env = (key, defaultValue) => {
+	if (!key in process.env) {
+		if (defaultValue) return defaultValue;
+		throw Error(`${key} not found in ENV variables!`);
+	}
 
-    return process.env[key];
-}
-const exists = async (value, options) =>
-{
-    const model = path.resolve(__dirname, "../models", options.model);
-    const Model = require(model);
-    var fieldName = options.field;
-    const modelQuery = await Model.find({[fieldName]: value});
-    if (!modelQuery.length) {
-        throw new Joi.ValidationError(
-            "ValidationError",
-            [
-                {
-                    message: `${options.model} do not exist`,
-                    path: [options.model],
-                //     type: "",
-                //     context: {
-                //         key: options.model,
-                //         label: options.model,
-                //         value,
-                //     },
-                },
-            ],
-            value
-        );
-    } 
-    return value;
-    
-}
+	// Access Cache if enabled
 
-const routerWrapper = (router) => 
-{
-    const _route = router.route.bind(router);
-    const methodsToWrap = ['get', 'post', 'patch', 'put', 'delete'];
-    router.route = function (path) {
-        const route = _route(path);
-        for (const method of methodsToWrap) {
-            if (route[method]) {
+	return process.env[key];
+};
+const exists = async (value, options) => {
+	const model = path.resolve(__dirname, '../models', options.model);
+	const Model = require(model);
+	var fieldName = options.field;
+	const modelQuery = await Model.find({ [fieldName]: value });
+	if (!modelQuery.length) {
+		throw new Joi.ValidationError(
+			'ValidationError',
+			[
+				{
+					message: `${options.model} do not exist`,
+					path: [options.model],
+					//     type: "",
+					//     context: {
+					//         key: options.model,
+					//         label: options.model,
+					//         value,
+					//     },
+				},
+			],
+			value
+		);
+	}
+	return value;
+};
 
-                route[method] = routeContainer(route[method]);
-            }
-        }
-        return route;
-    };
-}
+const routerWrapper = (router) => {
+	const _route = router.route.bind(router);
+	const methodsToWrap = ['get', 'post', 'patch', 'put', 'delete'];
+	router.route = function (path) {
+		const route = _route(path);
+		for (const method of methodsToWrap) {
+			if (route[method]) {
+				route[method] = routeContainer(route[method]);
+			}
+		}
+		return route;
+	};
+};
 
-const routeContainer = (originRouterMethod) => 
-{
-    return function () {
-        const originMiddlewares = [...arguments];
-        const wrappedMiddlewares = originMiddlewares.map(fn => {
-            if (typeof fn !== `function`) {
-                return fn;
-            }
+const routeContainer = (originRouterMethod) => {
+	return function () {
+		const originMiddlewares = [...arguments];
+		const wrappedMiddlewares = originMiddlewares.map((fn) => {
+			if (typeof fn !== `function`) {
+				return fn;
+			}
 
-            return async function (req, res, next) {
-                try {
-                    await fn.apply(null, arguments);
-                } catch (err) {
-                    next(err);
-                }
-            };
-        });
-        originRouterMethod.call(this, wrappedMiddlewares);
-    };
-}
+			return async function (req, res, next) {
+				try {
+					await fn.apply(null, arguments);
+				} catch (err) {
+					next(err);
+				}
+			};
+		});
+		originRouterMethod.call(this, wrappedMiddlewares);
+	};
+};
+
+const settings = async (type, userId, defaultSetting = null) => {
+	const Setting = require(path.join(process.cwd(), '/src/app/models/Setting'));
+	let settingObject;
+
+	if (userId === 'app-settings') {
+		settingObject = await Setting.findOne({
+			kind: 'app-settings',
+		}).lean();
+	} else {
+		settingObject = await Setting.findOne({
+			user: userId,
+		}).lean();
+	}
+	var settingKeyPair = {
+		name: type,
+		value: defaultSetting,
+	};
+	if (!settingObject?.configurations) return settingKeyPair;
+	settingKeyPair.value = settingObject.configurations[type];
+	return settingKeyPair;
+};
 
 module.exports = {
-    getPath,
-    env,
-    routerWrapper
-}
+	getPath,
+	settings,
+	env,
+	routerWrapper,
+};
